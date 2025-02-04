453db28a0b0e6836099d072880c8bd6917b98461
Nohop09 <ivan.gusev04@mail.ru>
2025-02-05 02:18:20 +0300

    feat: update database schema and add some game logic

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,16 +1,44 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 async function main() {
+  const user = await prisma.user.create({
+    data: {
+      login: "user",
+      rating: 1000,
+      passwordHash: "$2b$10$a4u7r0h39i58jX1x3o2k5O4e69a7c3897798998",
+    },
+  })
+  const user2 = await prisma.user.create({
+    data: {
+      login: "user2",
+      rating: 1000,
+      passwordHash: "$2b$10$a4u7r0h39i58jX1x3o2k5O4e69a7c3897798998",
+    },
+  })
   await prisma.game.create({
     data: {
-      name: "game-1",
+      field: Array(9).fill(null),
+      status: 'idle',
+      players: {
+        connect: {
+          id: user.id,
+
+        }
+      }
     },
-  });
+  })
   await prisma.game.create({
     data: {
-      name: "game-2",
+      field: Array(9).fill(null),
+      status: 'idle',
+      players: {
+        connect: {
+          id: user2.id,
+
+        }
+      }
     },
-  });
+  })
 }
 main()
   .then(async () => {
---
+++ src/entities/game/services/create-game.ts
@@ -0,0 +1,26 @@
+import { gameRepository } from "../repositories/game"
+import {PlayerEntity } from "../domain"
+import cuid from 'cuid'
+export  async function createGame(player: PlayerEntity) {
+    const playerGames = await gameRepository.gamesList({
+        players: {some: {id:player.id}},
+        status: 'idle',
+    })
+
+    const isGameInIdleStatus = playerGames.some((game) => game.status === 'idle' && game.creator.id === player.id,
+   ) 
+   if (isGameInIdleStatus)    {
+      return {
+        type: 'error',
+        error: 'can-create-only-one-game'
+    } as const 
+}
+    const createdGame = await gameRepository.createGame({
+        id: cuid(),
+        creator: player,
+        status: 'idle',
+       }
+    )
+
+    return createdGame 
+}
